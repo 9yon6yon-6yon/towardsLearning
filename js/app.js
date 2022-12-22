5c4aaa744e90b15c3c070f92579925a1bcec9e5a
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2022-12-23 00:02:39 +0600

    deleted files

--- js/app.js
+++
@@ -1,8 +0,0 @@
-function closeTreactPopup(){ 
-  document.querySelector(".treact-popup").classList.add("hidden");
-}
-function openTreactPopup(){ 
-  document.querySelector(".treact-popup").classList.remove("hidden");
-}
-document.querySelector(".close-treact-popup").addEventListener("click", closeTreactPopup);
-setTimeout(openTreactPopup, 3000)
